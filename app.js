509d0e55b22ff86fb2538533773b0c10ddfe8115
Jarquise Turner <j.turner348@gmail.com>
2021-08-20 04:24:04 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,42 +1,17 @@
-// Game begins
-const game = () => {
-    let pScore = 0;
-    let cScore = 0;
-    
-    //Play Match
-    const playMatch = () => {
-      const options = document.querySelectorAll(".options button");
-      const playerHand = document.querySelector(".player-hand");
-      const computerHand = document.querySelector(".computer-hand");
-  
-        }
-      
-      
-      //Computer array a.k.a options
-      const computerOptions = ["rock", "paper", "scissors"];
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
 
-      
+function getComputerChoice() {
+    const choices = ['r', 'p', 's'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
 
-      options.foreach(option =>{
-            option.addEventListener('click',function() {
-                const computerNumber = Math.floor(Math.random() * 3);
-                const computerChoice =computerOptions[computerNumber];
-                // Here is where we call compare hand
-            });
-      });
-const compareHands = (playerChoice, computerChoice) =>{
-    // update Text
-    const winner = document.queryselector('.winner')
-if(playerChoice === computerChoice){
-    winner.textContent = 'It is a tie'
-    return;
-}
-// if rock is choosen
-if(playerChoice === 'rock'){
-    if(computerChoice === 'scissors'){
-        winner.textcontent = 'Player Wins'
-    }
-}
-}
-    }  
-    playMatch()
+// 
