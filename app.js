f45caba81316934e26a165642e7f546a2050d511
Jarquise Turner <j.turner348@gmail.com>
2021-08-20 02:36:32 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,42 @@
+// Game begins
 const game = () => {
-    let playerScore = 0;
-    let computerScore = 0;
+    let pScore = 0;
+    let cScore = 0;
+    
+    //Play Match
+    const playMatch = () => {
+      const options = document.querySelectorAll(".options button");
+      const playerHand = document.querySelector(".player-hand");
+      const computerHand = document.querySelector(".computer-hand");
   
-    //Start the Game
-   
+        }
+      
+      
+      //Computer array a.k.a options
+      const computerOptions = ["rock", "paper", "scissors"];
+
+      
+
+      options.foreach(option =>{
+            option.addEventListener('click',function() {
+                const computerNumber = Math.floor(Math.random() * 3);
+                const computerChoice =computerOptions[computerNumber];
+                // Here is where we call compare hand
+            });
+      });
+const compareHands = (playerChoice, computerChoice) =>{
+    // update Text
+    const winner = document.queryselector('.winner')
+if(playerChoice === computerChoice){
+    winner.textContent = 'It is a tie'
+    return;
+}
+// if rock is choosen
+if(playerChoice === 'rock'){
+    if(computerChoice === 'scissors'){
+        winner.textcontent = 'Player Wins'
+    }
+}
+}
+    }  
+    playMatch()
